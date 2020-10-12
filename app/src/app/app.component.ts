2121558fa980a8a319f10561e2e71a6b88cc4467
tyage <namatyage@gmail.com>
2020-10-12 18:00:45 +0900

    fetch

--- app/src/app/app.component.ts
+++ app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(private http: HttpClient) {
+    http.get('/test').subscribe((body: string) => {
+      this.title = body
+    })
+  }
 }
